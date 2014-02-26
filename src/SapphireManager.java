5c5d123388ef32933c0a653dc7a87ab08ed64e24
dexots <dexots@localhost>
2014-02-26 21:47:25 +0800

    Merged with CD

--- src/SapphireManager.java
+++ src/SapphireManager.java
@@ -1,7 +1,12 @@
-import java.lang.Object.*;
-import java.util.*;
-import java.io.*;
-import java.text.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Scanner;
 
 /**
  * @author 
