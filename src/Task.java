63b1ade2e2d80ba17a3b24fca0cbb419f864aacf
dexots <dexots@localhost>
2014-03-09 19:09:22 +0800

    updated Task class file

--- src/Task.java
+++ src/Task.java
@@ -1,88 +1,86 @@
 class Task {
 	
-	private String startTime;
-	private String endTime;
-	private String deadline;
-	private String location;
-	private String taskName;
-	private String date;
-	private String type;
+	private String type, name, date, startTime, endTime, deadline, location;
 	
-	public Task(Task newTask){
-		this.startTime = newTask.startTime;
-		this.endTime = newTask.endTime;
-		this.deadline= newTask.deadline;
-		this.location = newTask.location;
-		this.taskName = newTask.taskName;
-		this.date = newTask.date;
-		this.type = newTask.type;
-				
+	public Task() {
 	}
 	
-	public Task(String type, String taskName, String date, String startTime,
-			String endTime, String deadline, String location){
-		this.type     = type;
-		this.taskName = taskName;
-		this.date     = date;
+	public Task(Task newTask){
+		setType(newTask.getType());
+		setName(newTask.getName());
+		setDate(newTask.getDate());
+		setStartTime(newTask.getStartTime());
+		setEndTime(newTask.getEndTime());
+		setDeadline(newTask.getDeadline());
+		setLocation(newTask.getLocation());		
+	}
+	
+	public Task(String type, String name, String date, String startTime, String endTime, String deadline, String location){
+		setType(type);
+		setName(name);
+		setDate(date);
+		setStartTime(startTime);
+		setEndTime(endTime);
+		setDeadline(deadline);
+		setLocation(location);
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setDate(String date){
+		this.date = date;
+	}
+
+	public void setStartTime(String startTime){
 		this.startTime = startTime;
-		this.endTime  = endTime;
+	}
+	
+	public void setEndTime(String endTime){
+		this.endTime = endTime;
+	}
+	
+	public void setDeadline(String deadline){
 		this.deadline = deadline;
+	}
+
+	public void setLocation(String location){
 		this.location = location;
 	}
 
-	public Task(){
-	}
-	
-	public void setTaskName(String name){
-		taskName = name;
-	}
-	public void setDate(String day){
-		date = day;
-	}
-
-	public void setStartTime(String time){
-		startTime = time;
-	}
-	
-	public void setEndTime(String time){
-		endTime = time;
-	}
-	
-	public void setDeadline(String time){
-		deadline = time;
-	}
-
-	public void setLocation(String loc){
-		location = loc;
-	}
-
-	public void setType(String typeOfTask){
-		type = typeOfTask;
+	public void setType(String type){
+		this.type = type;
 	}
 	
 	public String getName() {
-		return taskName;
+		return name;
 	}
+	
 	public String getDate(){
 	    return date;
 	}
+	
 	public String getStartTime(){
 		return startTime;
 	}
+	
 	public String getEndTime(){
 		return endTime;
 	}
+	
 	public String getDeadline(){
 		return deadline;
 	}
+	
 	public String getLocation(){
 		return location;
 	}
+	
 	public String getType(){
 		return type;
 	}
 	
-	public void print(Object line){
+	public void println(String line){
 		System.out.println(line);
 	}
 
@@ -90,20 +88,22 @@ class Task {
 		//Print task name which all tasks will have, followed by date/time if any,
 		//followed by other optional variables
 		
-		print(taskName);
+		println(getName());
 		
-		if(type.compareTo("fullDay")==0){
-			print("Date: " + date);
-		}else if(type.compareTo("setDuration")==0){
-			print("Date: " + date);
-			print("Time: " + startTime + " to " + endTime);
-		}else if(type.compareTo("targetedTime")==0){
-			print("Date: " + date);
-			print("Time: " + deadline);
+		if(getType().equals("fullDay")){
+			println("Date: " + getDate());
+		} else if(type.equals("setDuration")){
+			println("Date: " + getDate());
+			println("Time: " + getStartTime() + " to " + getEndTime());
+		} else if(type.equals("targetedTime")){
+			println("Date: " + getDate());
+			println("Time: " + getDeadline());
+		} else {
+			println("Does not belong to any category, OMG!");
 		}
 		
-		if(location!=null){
-			print("Location: " + location);
+		if(getLocation() != null){
+			println("Location: " + getLocation());
 		}
 	}
 
