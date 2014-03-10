cdeea563d2016d199ef49f3a7c711ea126f30d39
Sai Hou <Sai Hou@localhost>
2014-03-10 21:58:20 +0800

    Refactored Executor a little, added stubs for "clear", fixed  printTodaysTask

---
+++ src/Executor.java
@@ -0,0 +1,266 @@
+import java.util.ArrayList;
+
+/**
+ *	This class is the main executor that will execute the commands
+ *	ADD, DISPLAY, EDIT, DELETE, UNDO
+ */
+
+public class Executor {
+	
+	Storage taskStorage;
+	Parser parser;
+	UserInterface userInterface;
+	History history;
+	ArrayList<Task> allTasks;
+	
+	/**
+	 * @author Sai Hou
+	 */
+	public Executor(Parser parserFromManager, UserInterface ui) {
+		parser = parserFromManager;
+		userInterface = ui;
+		history = new History();
+		taskStorage = new Storage(ui);
+		allTasks = taskStorage.getTaskList();
+	}
+	
+	public void executeAddCommand(String userCommand) {
+		Task taskToBeAdded = new Task();
+		
+		parser.parse(userCommand, taskToBeAdded);
+		
+		boolean isAdditionOfNewTaskSuccessful = addThisTask(taskToBeAdded);
+		
+		if (isAdditionOfNewTaskSuccessful) {
+			userInterface.displayMessage("Successfully added : " + taskToBeAdded.getName() + ".");
+			updateHistory("add", taskToBeAdded, null);
+		}
+	}
+	
+	private boolean addThisTask(Task taskToBeAdded) {
+		allTasks.add(taskToBeAdded);
+		
+		boolean isFileWritingSuccessful = taskStorage.writeATaskToFile(taskToBeAdded, true);
+		return isFileWritingSuccessful;
+	}
+
+	public void executeDisplayCommand(String userCommand){
+		userInterface.displayTasksGivenList(allTasks);
+	}
+	
+	public void executeEditCommand(String userCommand){
+		//search for the task
+		ArrayList<Task> matchedTasks = searchByName(userCommand);
+		
+		if (searchResultIsEmpty(userCommand, matchedTasks)) {
+			return;
+		}
+		//display matched tasks
+		userInterface.displayExistingTasksFound(matchedTasks);
+		
+		//ask user to choose
+		int choice = loopUntilUserEntersValidChoice(matchedTasks);
+		
+		Task taskToBeEdited = matchedTasks.get(choice-1);
+		
+		//display selected task to user
+		userInterface.displayCurrentlyEditingSequence(taskToBeEdited);
+		
+		//read input from user, parse input
+		String userModifications = userInterface.readUserUpdates();
+		
+		Task duplicatedOldTask = new Task(taskToBeEdited);
+		
+		parser.parse(userModifications, taskToBeEdited);
+		
+		userInterface.displayMessage("Successfully made changes to " + taskToBeEdited.getName() +".");
+		
+		updateHistory("edit", taskToBeEdited, duplicatedOldTask);
+	}
+
+	public void executeDeleteCommand(String userCommand) {
+		//search for the task
+		ArrayList<Task> matchedTasks = searchByName(userCommand);
+		
+		if (searchResultIsEmpty(userCommand, matchedTasks)) {
+			return;
+		}
+		//display matched tasks
+		userInterface.displayExistingTasksFound(matchedTasks);
+		
+		//ask user to choose
+		int choice = loopUntilUserEntersValidChoice(matchedTasks);
+		
+		//retrieve the task to be removed
+		Task taskToBeDeleted = matchedTasks.get(choice-1);
+		
+		boolean isDeletionSuccessful = deleteThisTask(taskToBeDeleted);
+		
+		if (isDeletionSuccessful) {
+			userInterface.displayMessage("Successfully deleted "+choice+". "+taskToBeDeleted.getName());
+			updateHistory("delete", taskToBeDeleted, null);
+		}
+	}
+
+	private boolean searchResultIsEmpty(String userCommand, ArrayList<Task> matchedTasks) {
+		if (matchedTasks.size() == 0) {
+			userInterface.displayMessage("Cannot find " + userCommand + "!");
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	private boolean deleteThisTask(Task taskToBeRemoved) {
+		//delete from array list
+		allTasks.remove(taskToBeRemoved);
+		
+		//delete from text file
+		boolean isFileWritingSuccessful = taskStorage.writeTaskListToFile();
+		return isFileWritingSuccessful;
+	}
+	
+	//stub.. sort of.
+	private int loopUntilUserEntersValidChoice(ArrayList<Task> matchedTasks) {
+		int choice = userInterface.readUserChoice();
+		boolean isChoiceValid = parser.checkIfUserChoiceIsValid(matchedTasks);
+		
+		while (!isChoiceValid) {
+			userInterface.displayTasksGivenList(matchedTasks);
+			choice = userInterface.readUserChoice();
+			isChoiceValid = parser.checkIfUserChoiceIsValid(matchedTasks);
+		}
+		return choice;
+	}
+	
+	public void executeClearCommand(String userCommand){
+		//stub
+	}
+	
+	/**
+	 * @author executeUndoCommand and related functions: Si Rui 
+	 * Undo the last entered event. This can only be entered after an “add”, “delete”, or “edit” command.
+	 * 		
+	 * 		Last user action | Action taken by undo function
+	 * 		Added a task 	-> Remove the task
+	 * 		Deleted a task 	-> Restore the original task
+	 * 		Edited a task	-> Revert all changes done to the original task
+	 */
+	
+	private boolean noLastAction(String lastAction, boolean undoWasCalled){
+		if(lastAction == null && !undoWasCalled){	//undo was not called
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	private boolean ableToUndo(String lastAction){
+		boolean undoWasCalled = history.getUndoWasCalled();
+		
+		if(noLastAction(lastAction, undoWasCalled)){
+			userInterface.displayMessage("There is nothing to undo.");
+			return false;
+		}else if(undoWasCalled){
+			userInterface.displayMessage("Sorry, only able to undo once.");
+			return false;
+		}else{
+			return true;
+		}
+	}
+	
+	private String executeActionRequiredToUndo(String lastAction){
+		Task pointerToLastTask = history.getPointerToLastTask();
+		String taskName = pointerToLastTask.getName();
+		String actionTakenToUndo = "";
+		
+		switch(lastAction){
+			case "add": 
+				//execute delete(pointerToLastTask)
+				deleteThisTask(pointerToLastTask);
+				
+				actionTakenToUndo = "deleted \"" + taskName + '"';
+				break;
+			case "delete":	
+				//execute add(pointerToLastTask)
+				addThisTask(pointerToLastTask);
+				
+				actionTakenToUndo = "re-added \"" + taskName + '"';
+				break;
+			case "edit": 	//revert original copy of task
+				Task taskToRestore = history.getCopyOfLastTask();
+				//execute delete(pointerToLastTask)
+				deleteThisTask(pointerToLastTask);
+				//execute add(taskToRestore)
+				addThisTask(taskToRestore);
+				
+				actionTakenToUndo = "reverted \"" + taskName + '"';
+				break;
+		}
+		
+		return actionTakenToUndo;
+	}
+	
+	public void executeUndoCommand(){
+		String lastAction = history.getLastAction();
+		
+		if(ableToUndo(lastAction)){
+			String actionTakenToUndo = executeActionRequiredToUndo(lastAction);
+			userInterface.displayMessage("Successfully " + actionTakenToUndo + '.');
+		}
+		
+	}
+	
+	/**
+	 * @author Sai Hou
+	 */
+	private ArrayList<Task> searchByName(String name) {
+		ArrayList<Task> matchedTasks = new ArrayList<Task>();
+		
+		for (Task t : allTasks) {
+			String taskNameInLowerCase = t.getName().toLowerCase();
+			name = name.toLowerCase();
+			
+			if (taskNameInLowerCase.contains(name)) {
+				matchedTasks.add(t);
+			}
+		}
+		return matchedTasks;
+	}
+	
+	/**
+	 * @author Si Rui
+	 */
+	private ArrayList<Task> searchByDate(String date) {
+		ArrayList<Task> matchedTasks = new ArrayList<Task>();
+		
+		for (Task t : allTasks) {
+			String taskDate = t.getDate();
+			
+			if (taskDate != null) {
+				if (taskDate.compareTo(date)==0) {
+					matchedTasks.add(t);
+				}
+			}
+		}
+		return matchedTasks;
+	}
+	
+	public ArrayList<Task> getTodaysTasks(String date){
+		
+		ArrayList<Task> matchedTasks = searchByDate(date);
+		
+		return matchedTasks;
+	}
+	
+	/**
+	 * @author Sai Hou
+	 */
+	private void updateHistory(String lastAction, Task pointer, Task duplicatedTask) {
+		history.setLastAction(lastAction);
+		history.setPointerToLastTask(pointer);
+		history.setCopyOfLastTask(duplicatedTask);
+		history.setUndoIsCalled(false);
+	}
+}
---
+++ src/Storage.java
@@ -0,0 +1,260 @@
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ * @author Teck Sheng (Dex)
+ * This class is the storage that that deals with the reading of file and writing to file
+ */
+
+public class Storage {
+	private final static String FILE_NAME = "mytextfile.txt";
+	
+	private ArrayList<Task> taskList;
+	private BufferedReader bufferedReader;
+	private BufferedWriter bufferedWriter;
+	private File file, newFile;
+	private String fileName = FILE_NAME;
+	private Task task;
+	private UserInterface userInterface;
+
+	public Storage(UserInterface userInterface) {
+		this.userInterface = userInterface;
+		taskList = new ArrayList<Task>();
+		readFromFile();
+	}
+
+	public ArrayList<Task> getTaskList() {
+		return taskList;
+	}
+
+	public boolean setTaskList(Task task) {
+		if(task == null) {
+			return false;
+		} else {
+			taskList.add(task);
+			return true;
+		}
+	}
+	
+	/*
+	public void printArrayList() {
+		Task task1;
+		for(int i=0; i<taskList.size(); i++) {
+			task1 = taskList.get(i);
+			System.out.println("No: "+i+
+								"\nType: "+task1.getType()+
+								"\nName: "+task1.getName()+
+								"\nDate: "+task1.getDate()+
+								"\nStart Time: "+task1.getStartTime()+
+								"\nEnd Time: "+task1.getEndTime()+
+								"\nDeadline: "+task1.getDeadline()+
+								"\nLocation: "+task1.getLocation()+"\n");
+		}
+	}
+	*/
+	
+	public boolean readFromFile() {		
+		try {
+			file = new File(fileName);
+			if(file.exists()) {
+				userInterface.displayMessage("[Test Message]: File exists."); 
+				bufferedReader = new BufferedReader(new FileReader(fileName));
+				String line = bufferedReader.readLine();
+				String[] splitedTaskInfo;
+
+				while(line != null) {
+					splitedTaskInfo = line.split("\\|");
+					taskList.add(convertStringToTask(splitedTaskInfo));
+					line = bufferedReader.readLine();
+				}
+				bufferedReader.close();
+				userInterface.displayMessage("[Test Message]: File initialized.");
+				return true;
+			} else {
+				userInterface.displayMessage("[Test Message]: File did not exist."); 
+				bufferedWriter = new BufferedWriter(new FileWriter(file, true));
+				bufferedWriter.close();
+				userInterface.displayMessage("[Test Message]: File created."); 
+				return false;
+			}
+		} catch(Exception ex) {
+			userInterface.displayMessage("Error reading file! Please re-start program."); 
+			return false;
+		}
+	}
+
+	//toAppend: 
+	//true - append to existing file
+	//false - rewrite the file
+	public boolean writeATaskToFile(Task task, boolean toAppend) {
+		try {
+			bufferedWriter = new BufferedWriter(new FileWriter(new File(fileName), toAppend));
+			bufferedWriter.write(convertTaskToString(task));
+			bufferedWriter.newLine();
+			bufferedWriter.close();
+			return true;
+		} catch(Exception ex) {
+			userInterface.displayMessage("Error writing to file!");
+			return false;
+		}
+	}
+
+	public boolean writeTaskListToFile() {
+		if(taskList.size() == 0) { 
+			if(clearFile()) {
+				return true;
+			} else {
+				return false;
+			}
+		} else if(taskList.size() > 0) { //write text file
+			for(int i=0; i<taskList.size(); i++) {
+				if(i == 0) {
+					writeATaskToFile(taskList.get(i), false);
+				} else {
+					writeATaskToFile(taskList.get(i), true);
+				}
+			}
+			return true;
+		} else { 
+			userInterface.displayMessage("Error writing to file!");
+			return false;
+		}
+	}
+
+	public boolean clearFile() {
+		try {
+			file = new File(fileName);
+			if(file.delete()) {
+				newFile = new File(fileName);
+				if(newFile.createNewFile()) {
+					return true;
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		} catch (Exception ex) {
+			userInterface.displayMessage("Error deleting file.");
+			return false;
+		}
+	}
+	
+	private void pr(String msg) {
+		System.out.println(msg);
+	}
+
+	private Task convertStringToTask(String[] splitedTaskInfo) {
+		task = new Task();	
+		pr(Integer.toString(splitedTaskInfo.length));
+		
+		if(!splitedTaskInfo[0].equals("[null]")) { //type
+			pr(splitedTaskInfo[0]);
+			task.setType(splitedTaskInfo[0]); 
+		} else {
+			task.setType(null);
+		}
+
+		if(!splitedTaskInfo[1].equals("[null]")) { //task name
+			task.setName(splitedTaskInfo[1]);
+		} else {
+			task.setName(null);
+		}
+
+		if(!splitedTaskInfo[2].equals("[null]")) { //date
+			task.setDate(splitedTaskInfo[2]);
+		} else {
+			task.setDate(null);
+		}
+
+		if(!splitedTaskInfo[3].equals("[null]")) { //start time
+			task.setStartTime(splitedTaskInfo[3]);
+		} else {
+			task.setStartTime(null);
+		}
+
+		if(!splitedTaskInfo[4].equals("[null]")) { //end time
+			task.setEndTime(splitedTaskInfo[4]);
+		} else {
+			task.setEndTime(null);
+		}
+
+		if(!splitedTaskInfo[5].equals("[null]")) { //deadline
+			task.setDeadline(splitedTaskInfo[5]);
+		} else {
+			task.setDeadline(null);
+		}
+
+		if(!splitedTaskInfo[6].equals("[null]")) { //location
+			task.setLocation(splitedTaskInfo[6]);
+		} else {
+			task.setLocation(null);
+		}
+
+		return task;
+	}
+
+	private String convertTaskToString(Task task) {
+		String convertedTask = "";
+
+		if(task.getType() == null) { //type
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getType();
+		}
+
+		convertedTask += "|";
+
+		if(task.getName() == null) { //name
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getName();
+		}
+
+		convertedTask += "|";
+
+		if(task.getDate() == null) { //date
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getDate();
+		}
+
+		convertedTask += "|";
+
+		if(task.getStartTime() == null) { //start time
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getStartTime();
+		}
+
+		convertedTask += "|";
+
+		if(task.getEndTime() == null) { //end time
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getEndTime();
+		}
+
+		convertedTask += "|";
+
+		if(task.getDeadline() == null) { //deadline
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getDeadline();
+		}
+
+		convertedTask += "|";
+
+		if(task.getLocation() == null) { //location
+			convertedTask += "[null]";
+		} else {
+			convertedTask += task.getLocation();
+		}
+		
+		return convertedTask;
+	}
+}
