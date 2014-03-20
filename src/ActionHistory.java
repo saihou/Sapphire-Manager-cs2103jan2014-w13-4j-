80b33b77c3e58db886f4d29c11175db4ee5379b7
Si Rui <Si Rui@localhost>
2014-03-21 01:52:16 +0800

    Implemented Comparable for for Task class so tasks can be sorted. Update history. Comment out unused storage method.

---
+++ src/ActionHistory.java
@@ -0,0 +1,42 @@
+/**
+ * @author Si Rui
+ *	This class stores the history of the last user action, a pointer to the last task 
+ *	that was added, edited or deleted, and a copy of the details of that last task. 
+ *	This class will be referred to by the undo function in the CommandExecutor class.
+ *
+ *	Possible requests: set and get lastAction, pointerToLastTask and copyOfLastTask
+ *
+ */
+public class ActionHistory {
+	private String lastAction;
+	private Task referenceToLastTask;
+	private Task copyOfLastTask;
+	
+	public ActionHistory(){
+		this.lastAction = null;
+	}
+	
+	public void setLastAction(String lastAction){
+		this.lastAction = lastAction;
+	}
+	
+	public void setReferenceToLastTask(Task referenceToLastTask){
+		this.referenceToLastTask = referenceToLastTask;
+	}
+	
+	public void setCopyOfLastTask(Task copyOfLastTask){
+		this.copyOfLastTask = copyOfLastTask;
+	}
+	
+	public String getLastAction(){
+		return this.lastAction;
+	}
+	
+	public Task getReferenceToLastTask(){
+		return this.referenceToLastTask;
+	}
+	
+	public Task getCopyOfLastTask(){
+		return this.copyOfLastTask;
+	}
+}
