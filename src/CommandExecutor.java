191250cc75e7affe8f3264a3b890587a03469004
dexots <dexots@localhost>
2014-03-21 21:52:05 +0800

    updated GUI with new name attributes for GUI window, JFrame, JTextArea removed getInstance method as no class will be using it added JLabel as a help tip to improve user experience added more help commands to "Available Commands"

---
+++ src/CommandExecutor.java
@@ -0,0 +1,404 @@
+import java.util.ArrayList;
+
+/**
+ *	This class is the main executor that will execute the commands
+ *	ADD, DISPLAY, EDIT, DELETE, UNDO
+ */
+
+public class CommandExecutor {
+
+	protected Storage taskStorage;
+	protected CommandParser parser;
+	protected SapphireManagerGUI gui = SapphireManagerGUI.getInstance();
+	protected ActionHistory history;
+	protected ArrayList<Task> allTasks;
+	
+	protected String phase;
+	
+	protected Task currentTask;
+	protected ArrayList<Task> currentTaskList;
+	
+	/**
+	 * @author Sai Hou
+	 */
+	public CommandExecutor() {
+		parser = new CommandParser();
+		history = new ActionHistory();
+		taskStorage = new Storage();
+		allTasks = taskStorage.getTaskList();
+		phase = "normal";
+		currentTask = null;
+		currentTaskList = null;
+	}
+
+	public String executeAddCommand(String userCommand) {
+		String systemFeedback = "";
+		Task taskToBeAdded = new Task();
+
+		parser.parse(userCommand, taskToBeAdded);
+
+		boolean isAdditionOfNewTaskSuccessful = addThisTask(taskToBeAdded);
+
+		if (isAdditionOfNewTaskSuccessful) {
+			systemFeedback = "Successfully added : " + taskToBeAdded.getName() + ".";
+			updateHistory("add", taskToBeAdded, null);
+		}
+		return systemFeedback;
+	}
+
+	private boolean addThisTask(Task taskToBeAdded) {
+		allTasks.add(taskToBeAdded);
+
+		boolean isFileWritingSuccessful = taskStorage.writeATaskToFile(taskToBeAdded, true);
+		return isFileWritingSuccessful;
+	}
+
+	public void executeDisplayCommand(String userCommand){
+		gui.displayTasksGivenList(allTasks);
+	}
+
+	public String executeEditCommand(String userModifications){
+		String systemFeedback = "";
+
+		Task duplicatedOldTask = new Task(currentTask);
+
+		boolean isEditSuccessful = editThisTask(currentTask, userModifications);
+
+		if (isEditSuccessful) {
+			systemFeedback = "Successfully made changes to " + currentTask.getName() +".";
+			updateHistory("edit", currentTask, duplicatedOldTask);
+		}
+		return systemFeedback;
+	}
+	private boolean editThisTask(Task taskToBeEdited, String userModifications) {
+		//modify array list
+		parser.parse(userModifications, taskToBeEdited);
+
+		//write changes to file
+		boolean isFileWritingSuccessful = taskStorage.writeTaskListToFile();
+		return isFileWritingSuccessful;
+	}
+
+	public String executeDeleteCommand(int choice) {	
+		String systemFeedback = "";
+		//retrieve the task to be removed
+		currentTask = currentTaskList.get(choice-1);
+		
+		boolean isDeletionSuccessful = deleteThisTask(currentTask);
+		
+		if (isDeletionSuccessful) {
+			systemFeedback = "Successfully deleted "+choice+". "+currentTask.getName() + ".";
+			updateHistory("delete", currentTask, null);
+		}
+		return systemFeedback;
+	}
+	
+	private boolean deleteThisTask(Task taskToBeRemoved) {
+		//delete from array list
+		allTasks.remove(taskToBeRemoved);
+
+		//delete from text file
+		boolean isFileWritingSuccessful = taskStorage.writeTaskListToFile();
+		return isFileWritingSuccessful;
+	}
+
+	public String executeClearCommand() {
+		String systemFeedback = "";
+		taskStorage.clear();
+		systemFeedback = "Successfully cleared all tasks.";
+		return systemFeedback;
+	}
+
+	/**
+	 * @author executeUndoCommand and related functions: Si Rui 
+	 * Undo the last entered event. This can only be entered after an edit and delete
+	 * 		
+	 * 		Last user action | Action taken by undo function
+	 * 		Added a task 	-> Remove the task
+	 * 		Deleted a task 	-> Restore the original task
+	 * 		Edited a task	-> Revert all changes done to the original task
+	 */
+
+	private boolean ableToUndo(String lastAction) {
+		if(lastAction == null){
+			gui.printToDisplay("There is nothing to undo.");
+			return false;
+		} else if(lastAction.compareTo("undo") == 0) {
+			gui.printToDisplay("Sorry, only able to undo once.");
+			return false;
+		} else{
+			return true;
+		}
+	}
+
+	private String executeActionRequiredToUndo(String lastAction){
+		Task pointerToLastTask = history.getReferenceToLastTask();
+		String taskName = pointerToLastTask.getName();
+		String actionTakenToUndo = "";
+
+		switch(lastAction) {
+		case "add": 
+			//execute delete(pointerToLastTask)
+			deleteThisTask(pointerToLastTask);
+			actionTakenToUndo = "deleted \"" + taskName + '"';
+			break;
+		case "delete":	
+			//execute add(pointerToLastTask)
+			addThisTask(pointerToLastTask);
+			actionTakenToUndo = "re-added \"" + taskName + '"';
+			break;
+		case "edit": 	//revert original copy of task
+			Task taskToRestore = history.getCopyOfLastTask();
+			//execute delete(pointerToLastTask)
+			deleteThisTask(pointerToLastTask);
+			//execute add(taskToRestore)
+			addThisTask(taskToRestore);
+			actionTakenToUndo = "reverted \"" + taskName + '"';
+			break;
+		}
+
+		return actionTakenToUndo;
+	}
+
+	public void executeUndoCommand() {
+		String lastAction = history.getLastAction();
+
+		if(ableToUndo(lastAction)){
+			String actionTakenToUndo = executeActionRequiredToUndo(lastAction);
+			gui.printToDisplay("Successfully " + actionTakenToUndo + '.');
+			updateHistory("undo", null, null);
+		}
+
+	}
+
+	/**
+	 * @author Sai Hou
+	 */
+	public ArrayList<Task> searchByName(String name) {
+		ArrayList<Task> matchedTasks = new ArrayList<Task>();
+
+		for (Task t : allTasks) {
+			String taskNameInLowerCase = t.getName().toLowerCase();
+			name = name.toLowerCase();
+
+			if (taskNameInLowerCase.contains(name)) {
+				matchedTasks.add(t);
+			}
+		}
+		return matchedTasks;
+	}
+	
+	/**
+	 * @author Si Rui
+	 */
+	private ArrayList<Task> searchByDate(String date) {
+		ArrayList<Task> matchedTasks = new ArrayList<Task>();
+
+		for (Task t : allTasks) {
+			String taskDate = t.getDate();
+
+			if (taskDate != null) {
+				if (taskDate.compareTo(date) == 0) {
+					matchedTasks.add(t);
+				}
+			}
+		}
+		return matchedTasks;
+	}
+
+	public ArrayList<Task> getTodaysTasks(String date){
+
+		ArrayList<Task> matchedTasks = searchByDate(date);
+
+		return matchedTasks;
+	}
+
+	/**
+	 * @author Sai Hou
+	 */
+	private void updateHistory(String lastAction, Task reference, Task duplicatedTask) {
+		history.setLastAction(lastAction);
+		history.setReferenceToLastTask(reference);
+		history.setCopyOfLastTask(duplicatedTask);
+	}
+	
+	public String getOperation(String userCommand) {
+		return getFirstWord(userCommand);
+	}
+	
+	public String getPhase() {
+		return phase;
+	}
+	public ArrayList<Task> getCurrentTaskList() {
+		return currentTaskList;
+	}
+	public Task getCurrentTask() {
+		return currentTask;
+	}
+	public String doUserOperation(String userCommand) {
+		String systemFeedback = "";
+		
+		String operation = getOperation(userCommand);
+		
+		if(operation.equalsIgnoreCase("exit") || operation.equalsIgnoreCase("quit")) {
+			System.out.println("exit");
+			System.exit(0);
+		}
+		
+		switch (phase) {
+			case "normal" :
+				systemFeedback = executeOperation(operation, userCommand);
+				break;
+			case "editPhase1" :
+				int choice = convertToInteger(userCommand);
+				currentTask = currentTaskList.get(choice-1);
+				phase = "editPhase2";
+				break;
+			case "editPhase2" :
+				systemFeedback = executeEditCommand(userCommand);
+				phase = "normal";
+				break;
+			case "deletePhase1" :
+				choice = convertToInteger(userCommand);
+				systemFeedback = executeDeleteCommand(choice);
+				phase = "normal";
+				break;
+			default :
+				break;
+		}
+		
+		if (phase.equals("normal")) {
+			currentTask = null;
+			currentTaskList = null;
+		}
+		
+		return systemFeedback;
+	}
+	
+	private boolean checkIfOperationIsValid(String operation) {
+		boolean isValidOperation = false;
+		
+		if (operation.equalsIgnoreCase("add") || 
+			operation.equalsIgnoreCase("delete") ||
+			operation.equalsIgnoreCase("display") ||
+			operation.equalsIgnoreCase("edit") ||
+			operation.equalsIgnoreCase("undo") ||
+			operation.equalsIgnoreCase("clear")) {
+			isValidOperation = true;
+		}
+		return isValidOperation;
+	}
+	
+	private int convertToInteger(String userCommand) {
+		return Integer.parseInt(userCommand);
+	}
+	
+	private String executeOperation(String operation, String userCommand) {
+		String systemFeedback = "";
+		
+		boolean isValidOperation = checkIfOperationIsValid(operation);
+		
+		if (!isValidOperation) {
+			return "Wrong command entered! Enter F1 for a list of commands.";
+		}
+		
+		switch (operation) {
+			case "add" :
+				systemFeedback = executeAddCommand(userCommand.substring(4));
+				break;
+			case "display" :
+				executeDisplayCommand(userCommand.substring(7));
+				break;
+			case "edit" :
+				currentTaskList = searchByName(userCommand.substring(5));
+				phase = "editPhase1";
+				break;
+			case "delete" :
+				currentTaskList = searchByName(userCommand.substring(7));
+				phase = "deletePhase1";
+				break;
+			case "clear" :
+				systemFeedback = executeClearCommand();
+				break;
+			case "undo" :
+				executeUndoCommand();
+				break;
+			default : 
+				break;
+		}
+		return systemFeedback;
+	}
+	
+	private String getFirstWord(String str) {
+		return str.trim().split("\\s+")[0];
+	}
+	//only used for JUnitTesting!
+	
+	public String jUnitAutomatedTest() {
+		//basically combining all the println from various objects (UI, Task)
+		//and appending them to a StringBuilder. At the end, the StringBuilder is
+		//converted to a String and returned so that JUnit can assertEquals();
+
+		StringBuilder sb = new StringBuilder();
+		int count = 1;
+		for(Task task : allTasks){
+			sb.append(count+ ". ");
+
+			sb.append(task.getName()+"\n");
+
+			if(task.getType().equals("fullDay")){
+				sb.append("Date: " + task.getDate()+"\n");
+			} else if(task.getType().equals("setDuration")){
+				sb.append("Date: " + task.getDate()+"\n");
+				sb.append("Time: " + task.getStartTime() + " to " + task.getEndTime()+"\n");
+			} else if(task.getType().equals("targetedTime")){
+				sb.append("Date: " + task.getDate()+"\n");
+				//sb.append("Time: " + task.getDeadline()+"\n");
+			} else {
+				sb.append("To be completed during free-time.\n");
+			}
+
+			if(task.getLocation() != null){
+				sb.append("Location: " + task.getLocation()+"\n");
+			}
+
+			//task.printTaskDetails(count, gui);
+		}
+		return sb.toString();
+	}
+	
+	private void search(String taskName){
+		boolean searchDate = false;
+		taskName = taskName.trim();
+		
+		try{
+			int date = Integer.parseInt(taskName);
+			if(taskName.length()==6){
+				searchDate = true;
+			}
+			else{
+				searchDate = false;
+			}
+		}catch(NumberFormatException e){
+			searchDate = false;
+		}
+		
+		if(searchDate){
+			ArrayList<Task> matchedTasks = searchByDate(taskName);
+			//pls display matched task
+		}
+		else{
+			//search for the task
+			ArrayList<Task> matchedTasks = searchByName(taskName);
+			//pls display matched task
+			
+
+
+		}
+	}
+
+
+
+
+
+}
---
+++ src/SapphireManagerGUI.java
@@ -0,0 +1,440 @@
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.ComponentOrientation;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.Font;
+import java.awt.Frame;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.text.DefaultCaret;
+
+
+public class SapphireManagerGUI {
+	private final static String MESSAGE_CURRENTLY_EDITING = "Currently Editing:";
+	private final static String MESSAGE_HELP = "Enter F1 for a list of commands.";
+	private final static String MESSAGE_NO_TASK_TO_DISPLAY_TODAY = "You have no tasks due today.\n";
+	private final static String MESSAGE_WELCOME = "Welcome to Sapphire Manager!";
+	private final static String MESSAGE_TASKS_FOUND = "Existing tasks found: ";
+	private final static String MESSAGE_TODAY_TASK_TITLE = "Today's task:";
+	
+	private final static String PROMPT_FOR_NUMBER = "Enter a number: ";
+	private final static String PROMPT_FOR_EDITS = "Enter your edits: ";
+
+	private final static String SPLIT_LINE = "------------------------------------------";
+	
+	private final static String WRONG_COMMAND_ENTERED = "Wrong command entered! Enter F1 for a list of commands.";
+	
+	private static ArrayList<Task> todaysTasks;
+	private static CommandExecutor myExecutor;
+	private static SapphireManagerGUI guiWindow;
+	private static String todaysDate = "";
+	
+	//private static JOptionPane exitPane;
+	private static JTextArea displayBox;
+	
+	private JFrame guiFrame;
+	private JLabel helpTip;
+	private JScrollPane scrollPane;
+	private JTextField inputBox;
+	private Toolkit toolkit;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					todaysTasks = new ArrayList<Task>();
+					guiWindow = new SapphireManagerGUI();
+					todaysDate = getTodaysDate();
+
+					myExecutor = new CommandExecutor();
+					
+					todaysTasks = myExecutor.getTodaysTasks(todaysDate);
+					if(todaysTasks.isEmpty()) {
+						displayBox.append(MESSAGE_NO_TASK_TO_DISPLAY_TODAY);
+					} else {
+						if(todaysTasks.size() == 1) {
+							displayBox.append(MESSAGE_TODAY_TASK_TITLE);
+							//todaysTasks.get(0).printTaskDetails(1, window);
+						} else {
+							displayBox.append(MESSAGE_TODAY_TASK_TITLE);
+							for(int i=0; i<todaysTasks.size(); i++) {
+								//todaysTasks.get(i).printTaskDetails((i+1), window);
+							}
+						}
+					}
+
+					guiWindow.guiFrame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public SapphireManagerGUI() {	
+		initialize();
+		start();
+	}
+
+	private void initialize() {		
+		initializeSapphireManager();
+		initializeDisplayBoxInScrollPane();
+		initializeInputBox();
+		initializeHelpTip();
+		contentPaneDisplay();	
+	}
+
+	private void start() {
+		displayWelcomeMessage();
+		guiFrameListener();
+		textFieldListener();
+		displayBoxListener();
+	}
+
+	private void initializeSapphireManager() {
+		guiFrame = new JFrame();
+		guiFrame.getContentPane().setBackground(Color.WHITE);
+		guiFrame.setTitle("Sapphire Manager");
+		guiFrame.setBounds(100, 100, 300, 450);
+		guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		guiFrame.setResizable(false);
+		toolkit = Toolkit.getDefaultToolkit();
+		int x = toolkit.getScreenSize().width-guiFrame.getWidth();
+		int y = toolkit.getScreenSize().height-guiFrame.getHeight()-40;
+		guiFrame.setLocation(x, y);
+	}
+
+	private void initializeDisplayBoxInScrollPane() {
+		displayBox = new JTextArea(5, 30);
+		scrollPane = new JScrollPane(displayBox);
+
+		scrollPane.setBorder(null);
+		scrollPane.setPreferredSize(new Dimension(380, 380));
+		displayBox.setBackground(Color.WHITE);
+		displayBox.setBorder(null);
+		displayBox.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+		displayBox.setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+		displayBox.setEditable(false);
+		displayBox.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+		displayBox.setLineWrap(true);
+		displayBox.setMargin(new Insets(5, 5, 5, 5));
+		displayBox.setTabSize(2);
+		displayBox.setWrapStyleWord(true);
+
+		scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+	}
+
+	private void initializeInputBox() {
+		inputBox = new JTextField();
+		
+		inputBox.requestFocus();
+		inputBox.setPreferredSize(new Dimension(380, 50));
+		inputBox.setFont(new Font("Segoe UI", Font.PLAIN, 14));
+	}
+	
+	private void initializeHelpTip() {
+		helpTip = new JLabel();
+		
+		helpTip.setBackground(Color.WHITE);
+		helpTip.setLabelFor(inputBox);
+		helpTip.setText("helpo");
+	}
+
+	private void contentPaneDisplay() {
+		guiFrame.getContentPane().setLayout(new BoxLayout(guiFrame.getContentPane(), BoxLayout.Y_AXIS));
+		guiFrame.getContentPane().add(scrollPane, Component.CENTER_ALIGNMENT);
+		guiFrame.getContentPane().add(helpTip, Component.LEFT_ALIGNMENT);
+		guiFrame.getContentPane().add(inputBox, Component.CENTER_ALIGNMENT);
+	}
+
+	private void textFieldListener() {
+		inputBox.addKeyListener(new KeyAdapter() {			
+			@Override
+			public void keyReleased(KeyEvent e) {
+				updateScrollBar();
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					if(!inputBox.getText().trim().equals("")) {
+						String userCommand = readCommandFromUser();
+						
+						String systemFeedback = "";
+						String phase = "";
+						ArrayList<Task> matchedTasks = null;
+						
+						systemFeedback = myExecutor.doUserOperation(userCommand);
+						
+						phase = myExecutor.getPhase();
+						switch (phase) {
+							case "editPhase1" :
+								matchedTasks = myExecutor.getCurrentTaskList();
+								displayExistingTasksFound(matchedTasks);
+								break;
+							case "editPhase2" :
+								Task myTask = myExecutor.getCurrentTask();
+								displayCurrentlyEditingSequence(myTask);
+								break;
+							case "deletePhase1" :
+								matchedTasks = myExecutor.getCurrentTaskList();
+								displayExistingTasksFound(matchedTasks);
+								break;
+							default:
+								printToDisplay(systemFeedback);
+								break;
+						}
+						//printToDisplay(WRONG_COMMAND_ENTERED);
+						/*
+						 * String operation = getFirstWord(userCommand);
+						 */
+						/*
+						if(operation.equalsIgnoreCase("exit") || operation.equalsIgnoreCase("quit")) {
+							printToDisplay("Sapphire Manager will now exit.");
+							System.exit(0);
+						} else if(operation.equalsIgnoreCase("add")) {
+							myExecutor.executeAddCommand(userCommand.substring(4));
+						} else if(operation.equalsIgnoreCase("display")) {
+							myExecutor.executeDisplayCommand(userCommand.substring(7));
+						} else if(operation.equalsIgnoreCase("undo")) {
+							myExecutor.executeUndoCommand();
+						} else if(operation.equalsIgnoreCase("clear")) {
+							printToDisplay("All tasks cleared.");
+							myExecutor.executeClearCommand();
+						} else if(operation.equalsIgnoreCase("delete") && currentTask == "") {
+							currentTask = "delete";
+							matchedTasks = myExecutor.searchByName(userCommand.substring(7));
+							displayExistingTasksFound(matchedTasks);
+						} else if(currentTask.equalsIgnoreCase("delete")) {
+							myExecutor.executeDeleteCommand2(matchedTasks);
+							currentTask = "";
+						} else if(operation.equalsIgnoreCase("edit") && currentTask == "") {
+							currentTask = "edit1";
+							matchedTasks = myExecutor.searchByName(userCommand.substring(5));
+							displayExistingTasksFound(matchedTasks);
+						} else if(currentTask.equalsIgnoreCase("edit1")) {
+							currentTask = "edit2";
+							taskToBeEdited = myExecutor.executeEditCommand2(matchedTasks);
+							displayCurrentlyEditingSequence(taskToBeEdited);
+						} else if(currentTask.equalsIgnoreCase("edit2")) {
+							printToDisplay("Entered: '"+textField.getText()+"'.");
+							String userModifications = readUserEdits();
+							myExecutor.executeEditCommand3(taskToBeEdited, userModifications);
+							currentTask = "";
+						} else {
+							printToDisplay(WRONG_COMMAND_ENTERED);
+						}
+						*/
+						inputBox.setText("");
+					}
+				} else if(e.getKeyCode() == KeyEvent.VK_F1) {
+					printToDisplay("Help");
+					displayHelp();
+				} else if(e.getKeyCode() == KeyEvent.VK_F5) {
+					displayBox.setText("");
+				} else if(e.getKeyCode() == KeyEvent.VK_F2) {
+					guiFrame.setState(Frame.ICONIFIED);
+				} else if(e.getKeyCode() == KeyEvent.VK_F3) {
+					guiFrame.setState(Frame.NORMAL);
+				} else if(e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+					printToDisplay("Sapphire Manager will now exit.");
+					System.exit(0);
+					/*
+					int n = JOptionPane.showConfirmDialog(
+						    new Frame(),
+						    "Confirm exit?",
+						    "Exit confirmation",
+						    JOptionPane.YES_NO_OPTION);
+					System.out.println("n: "+n);
+					if(n == 0) {						
+						System.exit(1);
+					}*/
+				} else if(e.getKeyCode() == KeyEvent.VK_PAGE_UP) {
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getValue()-25);
+				} else if(e.getKeyCode() == KeyEvent.VK_PAGE_DOWN) {
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getValue()+25);
+				} else if(e.getKeyCode() == KeyEvent.VK_HOME) {
+					scrollPane.getVerticalScrollBar().setValue(0);
+				} else if(e.getKeyCode() == KeyEvent.VK_END) {
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getMaximum());
+				}
+			}
+
+			@Override
+			public void keyPressed(KeyEvent a) {
+				if(a.getKeyCode() == KeyEvent.VK_PAGE_UP) {
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getValue()-20);
+				} else if(a.getKeyCode() == KeyEvent.VK_PAGE_DOWN) {
+					scrollPane.getVerticalScrollBar().setValue(scrollPane.getVerticalScrollBar().getValue()+20);
+				}
+			}
+		});
+	}
+
+	private void guiFrameListener() {
+		guiFrame.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				inputBox.requestFocusInWindow();
+			}
+		});
+
+		guiFrame.addWindowFocusListener(new WindowAdapter() {
+			@Override
+			public void windowGainedFocus(WindowEvent e) {
+				inputBox.requestFocusInWindow();
+			}
+		});
+	}
+
+	private void displayBoxListener() {
+		displayBox.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent arg0) {
+				updateScrollBar();
+			}
+		});
+
+		displayBox.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				inputBox.requestFocusInWindow();
+				updateScrollBar();
+			}
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				inputBox.requestFocusInWindow();
+				updateScrollBar();
+			}
+		});
+	}
+
+	private void updateScrollBar() {
+		displayBox.setCaretPosition(displayBox.getDocument().getLength());
+		DefaultCaret caret = (DefaultCaret)displayBox.getCaret();  
+		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);  
+	}
+
+	public void printToDisplay(String message) {
+		displayBox.append(message+"\n");
+	}
+
+	public void displayWelcomeMessage() {
+		printToDisplay(MESSAGE_WELCOME);
+		printToDisplay("Today's date: "+getTodayDate());
+		printToDisplay("------------------------------------------");
+		printToDisplay(MESSAGE_HELP);
+		printToDisplay("------------------------------------------");
+	}
+
+	public void displayHelp() {
+		printToDisplay(SPLIT_LINE);
+		printToDisplay("Available Commands");
+		printToDisplay(SPLIT_LINE);
+		printToDisplay("1. add [task name] [options]");
+		printToDisplay("2. delete [task name (in part or in full)]");
+		printToDisplay("3. edit [task name (in part or in full)]");
+		printToDisplay("4. display [all | today | past | future]");
+		printToDisplay("5. undo");
+		printToDisplay("6. search [task name | category | date]");
+		printToDisplay("7. clear");
+		printToDisplay("8. exit | quit");
+		printToDisplay(SPLIT_LINE);
+		printToDisplay("- Options:");
+		printToDisplay("\tDate: '/on [date]'");
+		printToDisplay("\tTime: '/from [time] to [time]' or \n\t\t   '/at [time]'");
+		printToDisplay("\tLocation: '/loc [location name]'");
+		printToDisplay("\tCategory: '/c [one-word-name]'");
+		printToDisplay("\tReminder: '/r [time]'");
+
+	}
+	
+	public void displaySingleTask(Task taskToDisplay) {	
+		//taskToDisplay.printTaskDetails(1, window);
+	}
+
+	public void displayTasksGivenList(ArrayList<Task> taskList) {
+		if(taskList.size() <= 0) {
+			printToDisplay("No tasks available.");
+		} else {
+			printToDisplay("Tasks available:");
+			int count = 1;
+			for(Task task: taskList) {
+				//printToDisplay(count++ + ". ");
+				//task.printTaskDetails(count++, window);
+			}
+		}
+	}
+
+	public void displayExistingTasksFound(ArrayList<Task> taskList) {
+		if (taskList != null) {
+			printToDisplay(MESSAGE_TASKS_FOUND);
+			displayTasksGivenList(taskList);
+			printToDisplay(PROMPT_FOR_NUMBER);
+		}
+		else {
+			//printToDisplay(MESSAGE_NO_SEARCH_RESULTS);
+		}
+	}
+
+	public void displayCurrentlyEditingSequence(Task taskBeingEdited) {
+		printToDisplay(MESSAGE_CURRENTLY_EDITING);
+		displaySingleTask(taskBeingEdited);
+		printToDisplay(PROMPT_FOR_EDITS);
+	}
+
+	private String getTodayDate() {
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("dd-MMM-yyyy");
+		Calendar date = Calendar.getInstance();
+		return dateFormatter.format(date.getTime());
+	}
+
+	private static String getTodaysDate(){
+		SimpleDateFormat dateFormatter = new SimpleDateFormat("ddMMyy");
+		Calendar date = Calendar.getInstance();
+		return dateFormatter.format(date.getTime());
+	}
+
+	private String readUserInput() {
+		String userInput = null;
+		if(!inputBox.equals(null)) {
+			userInput = inputBox.getText();
+		}
+		return userInput;
+	}
+
+	public String readCommandFromUser() {
+		//printToDisplay(PROMPT_FOR_COMMAND);
+		return readUserInput();
+	}
+
+	public String readUserEdits() {
+		return readUserInput();
+	}
+
+	public int readUserChoice() {
+		//textField.setText("");
+		String userInput = readUserInput();
+		int userChoice = Integer.parseInt(userInput);
+		return userChoice;
+	}
+	
+}
